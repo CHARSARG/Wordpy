940963a700318c084004dd4d146355a16fd909d9
CHARSARG <66459808+CHARSARG@users.noreply.github.com>
2021-08-06 17:16:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'PrismPy',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  author_email = 'charsarg@hotmail.com',      # Type in your E-Mail
+  url = 'https://github.com/CHARSARG/Wordpy.git',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
@@ -19,9 +19,6 @@ setup(
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.9',      #Specify which pyhton versions that you want to support
   ],
 )
